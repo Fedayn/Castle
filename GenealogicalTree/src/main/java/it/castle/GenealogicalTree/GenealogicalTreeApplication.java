65638365d9a804798f362a45ec1468442d9c2c57
Francesco Mercoglian <Francesco Mercoglian@192.168.1.147>
2018-10-08 23:21:43 +0200

    First Commit

---
+++ GenealogicalTree/src/main/java/it/castle/GenealogicalTree/GenealogicalTreeApplication.java
@@ -0,0 +1,12 @@
+package it.castle.GenealogicalTree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenealogicalTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenealogicalTreeApplication.class, args);
+	}
+}
